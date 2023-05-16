1c9112f3b639509f8d5119896d7d72fa0e8e4276
wenderbritto <wenderbrito.dev@gmail.com>
2023-05-16 00:31:24 -0300

    Primeira parte do projeto

---
+++ ProjetoFinalAvaliacaoIII/src/Principal/GerenciadorBD.java
@@ -0,0 +1,44 @@
+package Principal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GerenciadorBD {
+    
+    private Connection conexao = null;
+    
+    private final String servidor = "127.0.0.1:3306";
+    private final String banco = "linguagemi";
+    private final String login = "root";
+    private final String senha = "1519";
+    
+    
+    public Connection Conectar(){
+        
+        try{
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            String textoConexao = "jdbc:mysql://" +this.servidor + "/" + this.banco;
+            
+            conexao = DriverManager.getConnection(textoConexao, this.login, this.senha);
+            
+            System.out.println("Conectou");
+        
+        }
+        catch (SQLException | ClassNotFoundException ex)
+        {
+            System.out.println("Erro");
+        
+        }
+        
+        return conexao;
+    
+    
+    
+    
+    }
+    
+    
+}
